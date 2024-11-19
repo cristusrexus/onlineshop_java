37d95b7268de768025de477185c41292ff4d7a8f
crisu <cristianmihaiandrei3@gmail.com>
2024-11-19 20:51:21 +0200

    first commit - setup

---
+++ src/test/java/com/onlineshop/onlineshop_java/OnlineshopJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineshop.onlineshop_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineshopJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
